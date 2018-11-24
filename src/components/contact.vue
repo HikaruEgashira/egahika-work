5466c6357b02a0faf1637429e806ac87d2aa372e
HikaruEgashira <shighten@gmail.com>
2018-11-25 01:05:57 +0900

    prefix; div class to container

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contact" style="position: relative; top: -40px">
+  <div class="container" style="position: relative; top: -40px">
     <img v-bind:src="require('../assets/contact.png')" alt="contact" fluid height="200px"><br><br>
     <h2>まだ作ってないので安全です()</h2>
   </div>
--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="profile" style="position: relative; top: -70px">
+  <div class="container" style="position: relative; top: -70px">
     <img v-bind:src="require('../assets/profile.png')" alt="profile" fluid height="200px"><br><br>
     <h2>自己紹介</h2>
     <br><br>
     <p>江頭輝です</p>
-    <p>`ひかる`と呼びます</p>
+    <p>'ひかる'と呼びます</p>
     <p><b-link href="https://github.com/HikaruEgashira">github</b-link></p>
     <p><b-link href="https://twitter.com/ITF_hikary">twitter</b-link></p>
   </div>
