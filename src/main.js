cc8a70fd1a0888263838d14f5cc51eaac2b67ac7
HikaruEgashira <shighten@gmail.com>
2018-11-29 17:26:06 +0900

    add: vuedarkmode

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueDarkMode from 'vuedarkmode'
 import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueDarkMode)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
